d33e669ad087a4e085e96be326a814141b257cc2
Felipe de Oliveira Silva <felipes@ciandt.com>
2021-02-09 22:58:16 -0300

    initial-configs

--- src/main/java/com/scrap/product/api/controllers/ScrapProductController.java
+++ src/main/java/com/scrap/product/api/controllers/ScrapProductController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,6 +29,18 @@ public class ScrapProductController {
 	@PutMapping(path = "/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.ACCEPTED)
 	public ResponseEntity<String> put(
+			@ApiParam(value = "URL to get details of products", required = true) @RequestParam(name = "url") final String url) {
+
+		LOGGER.debug("Received GET request");
+		return ResponseEntity.ok("String");
+	}
+
+	@ApiOperation(value = "get products", response = String.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 403, message = "Forbidden"), @ApiResponse(code = 404, message = "Not found") })
+	@GetMapping(path = "/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public ResponseEntity<String> get(
 			@ApiParam(value = "URL to get details of products", required = true) @RequestParam(name = "url") final String url) {
 
 		LOGGER.debug("Received GET request");
