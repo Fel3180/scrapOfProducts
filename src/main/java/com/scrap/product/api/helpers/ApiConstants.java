c9670f9fd3107f9e4212fefb188f3c4a0b2cdb53
Felipe de Oliveira Silva <felipes@ciandt.com>
2021-02-13 23:16:15 -0300

    Implementing-ttl-mongodb

--- src/main/java/com/scrap/product/api/helpers/ApiConstants.java
+++ src/main/java/com/scrap/product/api/helpers/ApiConstants.java
@@ -2,7 +2,6 @@ package com.scrap.product.api.helpers;
 
 public abstract class ApiConstants {
 
-
 	public static final String UNAUTHORIZED_ACCESS = "Unauthorized access";
 	public static final String ACCESS_DENIED = "Access denied";
 	public static final String API_TITLE = "Scrap Products Service";
@@ -11,6 +10,6 @@ public abstract class ApiConstants {
 	public static final String API_VERSION_1_LABEL = "API v1";
 
 	private ApiConstants() {
-		// customerDealsHelper class, only constants variables here
+
 	}
 }
